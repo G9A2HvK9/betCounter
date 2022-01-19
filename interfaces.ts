8dcf3995e573123f6289adedb1589737a2d6fa44
Hendrik Geiger <g9a2hvk9@gmail.com>
2022-01-19 18:01:42 +0100

    adds basic hello world server in deno

---
+++ interfaces.ts
@@ -0,0 +1,20 @@
+export interface IAdvertisement {
+    id: string;
+    name?: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+    isActive?: boolean;
+    type?: Array<IType>;
+    channel?: Array<IChannel>;
+}
+
+export interface IChannel {
+    id: string;
+    name: string;
+}
+
+export interface IType {
+    id: string;
+    name: string;
+}
